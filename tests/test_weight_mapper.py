811161a10cbf14627c75df5d7f59b38098812e00
Ben Y. <bjy777101@gmail.com>
2025-11-05 11:43:34 +1100

    Updated weight mapper tests to use a valid WeightSortMapper class
    Testcase was attempting to use non-existent class WeightSortMapper instead of a
    specified variant. Switched it to the LinearWeightSortMapper

--- tests/test_weight_mapper.py
+++ tests/test_weight_mapper.py
@@ -1,17 +1,17 @@
 import unittest
-from graph_state_generation.graph_state import graph_state, graph_node 
+from graph_state_generation.graph_state import graph_state, graph_node
 from graph_state_generation.mappers import weight_sort_mapper
 
 class WeightedMapperTest(unittest.TestCase):
 
-    def test_small_instance(self): 
-        small_graph = graph_state.GraphState(3) 
+    def test_small_instance(self):
+        small_graph = graph_state.GraphState(3)
 
         small_graph[0].append(*[1, 2])
         small_graph[1].append(*[0])
         small_graph[2].append(*[0])
 
-        mapper = weight_sort_mapper.WeightSortMapper(small_graph)
+        mapper = weight_sort_mapper.LinearWeightSortMapper(small_graph)
 
 
 if __name__ == '__main__':
